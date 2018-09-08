e32950691c01a81cd721f1b0381fac2aaa249ff9
Namoren <42718987+Namoren@users.noreply.github.com>
2018-09-08 07:20:17 +0200

    commands uploaded

---
+++ Commands/help.js
@@ -0,0 +1,11 @@
+console.log(`----help used----`);
+exports.run = (client, message, args) => {
+
+        if (message.content.startsWith(`_help`)) {
+
+        message.reply("Hilfe wurde gesendet.");
+        message.author.send("**Pizzacatbot** Befehle:\n\n__**Wichtige Befehle**__\n\n`_help` - Sendet dir diese Liste der verfügbaren Befehle.\n\n__**Fun Befehle**__\n\n`_beer` - Gibt jemandem ein Bier :beer: (`_beer` @user)\n`_handhold` - Halt mit jemand Händchen !(`_handhold` @user)\n`_highfive` - Gebt jemandem einen Highfive !(`_highfive` @user)\n`_flower` - Gebt jemandem eine Blume :rose: (`_flower` @user)\n`_cookieparty` - Startet eine Keksparty! oh yeah! [mit Gif].\n`_cookie` - Gib jemandem einen Keks yamyam ! (`_cookie` @user)[mit Gif]\n`_cuddle` - Kuschel mit jemandem ! (`_cuddle` @user)[mit Gif]\n`_kiss` - Sende jemandem einen Kuss :kiss: (`_kiss` @user)[mit Gif]\n`_lick` - Lecke jemanden ab :yum: (`_lick` @user)[mit Gif]\n`_hug` - Umarme jemanden :hugging: (`_hug` @user)[mit Gif]\n`_pat` - Tätschel jemanden :hand_splayed: (`_pat` @user)[mit Gif]\n`_slap` - Verpass jemandem eine Ohrfeige bam! (`_slap` @user)[mit Gif]\n`_tickle` - Kitzel jemanden durch.(`_tickle` @user)[mit Gif]\n\n__**Spiele Befehle**__\n\n`_8ball` - Stell der heiligen 8ball eine Frage ! (`_8ball` <Frage>)\n`_dice` - Rolle einen Würfel.\n`_slot` - Lass uns eine Runde Einarmiger bandit spielen. :D.\n`_rps` - Papier Stein Schere wer kennt das spiel nicht (Gegen CPU) ! (`_rps` papier stein oder schere).\n\n__**Musik Befehle**__\n**Bitte geht in den Musik Voice channel und gebt den Befehl nur im #Musik-Commands Channel ein!(anderswo funktionieren sie nicht!)**\n`~help` damit bekommt ihr eine Liste für alle Music Befehle(Die Befehle sind auch Angepinnt in dem Channel)\n\n\n\n\n\n ***Bitte habt noch etwas Gedult der Bot ist noch WIP***\n Ps: Wenn irgendwelche Fragen sind fragt Samala nicht Namoren der hat keine Ahnung :P ");
+        }
+          
+
+}
